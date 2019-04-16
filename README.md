4c3f3d2fae7d256d3f72117f48e21a517ac896fe
Victor Frank <vfrank@lumeris.com>
2019-04-16 09:56:45 -0500

    minor updates

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 [![Build Status](https://www.travis-ci.com/vfrank66/awsglue-local-dev.svg?branch=master)](https://www.travis-ci.com/vfrank66/awsglue-local-dev)
+
+# Shoutout to BDE2020
+
+Big thanks for all the setup material provided by https://github.com/big-data-europe/docker-spark.
+
 # awsglue-local-dev
-A local development setup for AWS Glue, modified fromhttps://github.com/big-data-europe/docker-spark
+
+A local development setup for AWS Glue, modified from https://github.com/big-data-europe/docker-spark
+
+This will build nightly due to how it pulls the AWS Glue scala .jar and the AWS Glue PyGlue.zip file from an s3 public repo, which is also the same jars included in the AWS Glue environment.
+
+
+# Example 
+
--- submit/submit.sh
+++ submit/submit.sh
@@ -3,6 +3,19 @@
 export SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
 export SPARK_HOME=/spark
 
+if [ -f "${SPARK_EXTRA_JARS_LOCATION}" ]; then
+    export AWS_GLUE_JAR="/app/lib/glue-assembly.jar"
+else 
+    export AWS_GLUE_JAR="/app/lib/glue-assembly.jar,${SPARK_EXTRA_JARS_LOCATION}"
+fi 
+
+if [ -f "${SPARK_APPLICATION_PYTHON_LOCATION}" ]; then
+    export AWS_PY_FILES="/app/lib/PyGlue.zip"
+else 
+    export AWS_PY_FILES="/app/lib/PyGlue.zip,${SPARK_EXTRA_PYTHON_FILES_LOCATION}"
+fi 
+
+
 
 if [ -f "${SPARK_APPLICATION_JAR_LOCATION}" ]; then
     echo "Submit application ${SPARK_APPLICATION_JAR_LOCATION} with main class ${SPARK_APPLICATION_MAIN_CLASS} to Spark master ${SPARK_MASTER_URL}"
@@ -10,7 +23,7 @@ if [ -f "${SPARK_APPLICATION_JAR_LOCATION}" ]; then
     /spark/bin/spark-submit \
         --class ${SPARK_APPLICATION_MAIN_CLASS} \
         --master ${SPARK_MASTER_URL} \
-        --jars /app/lib/glue-assembly.jar${SPARK_EXTRA_JARS_LOCATION} \
+        --jars ${AWS_GLUE_JAR} \
         ${SPARK_SUBMIT_ARGS} \
         ${SPARK_APPLICATION_JAR_LOCATION} ${SPARK_APPLICATION_ARGS}
 else
@@ -19,11 +32,11 @@ else
         echo "Passing arguments ${SPARK_APPLICATION_ARGS}"
         PYSPARK_PYTHON=python3 /spark/bin/spark-submit \
             --master ${SPARK_MASTER_URL} \
-            --py-files /app/lib/PyGlue.zip${SPARK_EXTRA_PYTHON_FILES_LOCATION} \
+            --py-files ${AWS_PY_FILES} \
             ${SPARK_SUBMIT_ARGS} \
             ${SPARK_APPLICATION_PYTHON_LOCATION} ${SPARK_APPLICATION_ARGS}
     else
-        echo "Not recognized application."
+        echo "[Error] Application Jar/Python not found, not recognized application."
     fi
 fi
 
