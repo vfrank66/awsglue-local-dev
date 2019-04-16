377b62f3accfcf8cc4558c6249b297e13a34ba04
Victor Frank <vfrank@lumeris.com>
2019-04-16 00:56:27 -0500

    fix build

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-
 set -e
 
 TAG=2.2.1-hadoop2.7
 
 build() {
+    NAME=$1
+    IMAGE=spark/spark-$NAME:$TAG
+    cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
+    echo '--------------------------' building $IMAGE in $(pwd)
+    docker build -t $IMAGE .
+    cd -
+}
+
+finalbuild() {
     NAME=$1
     IMAGE=vfrank66/awsgluespark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
@@ -16,4 +24,4 @@ build() {
 build base
 build master
 build worker
-build submit
+finalbuild submit
