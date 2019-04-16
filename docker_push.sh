7a632e1c14af871807ad18636a36bc45e08d445c
Victor Frank <vfrank@lumeris.com>
2019-04-16 01:13:55 -0500

    fix build

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push vfrank66/awsgluespark-submit:latest
+docker push vfrank66/awsgluespark-submit:2.2.1-hadoop2.7
